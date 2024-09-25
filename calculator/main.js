0aa325d48a8cef86d9915a67a65fdddfc9490268
Durlav Parajuli <durlove60@gmail.com>
2024-09-25 10:33:23 +0545

    calc

---
+++ calculator/main.js
@@ -0,0 +1,66 @@
+// const insert = (num) => {
+// 	document.getElementById("textview").value += num;
+// };
+
+// const clearAll = () => {
+// 	document.getElementById("textview").value = "";
+// };
+
+// const clearOne = () => {
+// 	const value = document.getElementById("textview").value;
+// 	document.getElementById("textview").value = value.substring(
+// 		0,
+// 		value.length - 1
+// 	);
+// };
+
+// // to verify the input
+// const isValidExpression = (expr) => {
+// 	try {
+// 		// Check if the expression is valid by evaluating it in a safe manner
+// 		// Using Function constructor to avoid eval security issues
+// 		new Function(`return ${expr}`);
+// 		return true;
+// 	} catch (e) {
+// 		return false;
+// 	}
+// };
+
+// const calculate = () => {
+// 	const value = document.getElementById("textview").value;
+// 	if (value) {
+// 		if (!isValidExpression(value)) {
+// 			alert("Invalid expression");
+// 			return;
+// 		}
+// 		document.getElementById("textview").value = eval("value");
+// 	}
+// };
+
+function insert(exp) {
+	const display = document.getElementById("textview");
+	display.value = display.value + exp;
+	// display.value += exp;
+}
+
+function clearAll() {
+	const display = document.getElementById("textview");
+	display.value = "";
+}
+
+function calculate() {
+	const display = document.getElementById("textview");
+	if (display.value === "") {
+		alert("Enter an expression");
+	} else {
+		display.value = eval(display.value);
+	}
+}
+
+const clearOne = () => {
+	const value = document.getElementById("textview").value;
+	document.getElementById("textview").value = value.substring(
+		0,
+		value.length - 1
+	);
+};
